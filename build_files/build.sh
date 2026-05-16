12978e1fed7a1baee014ccb832640577bed38917
Ivan Cheltsov <chip.pragma@gmail.com>
2026-05-16 05:09:46 +0300

    Use valid kernel version

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,19 +10,24 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux 
 
 ### rtw69 (morrownr/rtw89)
+
 # requirements (already installed)
-# dnf5 install -y make gcc kernel-devel kernel-headers 
-dnf5 install -y dkms 
+# dnf5 install -y make gcc kernel-devel kernel-headers
+
 # clone repo
 git clone https://github.com/morrownr/rtw89
 cd rtw89
+# prepare
+export KVER="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 # clean up
-# make cleanup_target_system
+make cleanup_target_system
+make clean 
 # build & install
-dkms install $(pwd)
+make modules
+make install
 make install_fw
 # configurate
 cp -v rtw89.conf /etc/modprobe.d/
@@ -39,4 +44,4 @@ rm -rf rtw89
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
